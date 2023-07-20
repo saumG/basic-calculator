29b4169459f73701f229db58e0ef6bb2d74155cd
saumG <saumster123@gmail.com>
2023-07-19 21:48:35 -0700

    created the subtract function, which subtracts num 2 from num 1

--- calculator.js
+++ calculator.js
@@ -1,6 +1,11 @@
 
 
-// ADD function, adds all values in array
+// ADD function, adds two nums
 function add(num1, num2) {
     return num1 + num2;
+}
+
+// SUBTRACT funciton, subtracts num2 from num1
+function subtract(num1, num2) {
+    return num1 - num2;
 }
