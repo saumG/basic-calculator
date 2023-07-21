8c32379cc593d06b33a2f9994c65736c3e03862d
saumG <saumster123@gmail.com>
2023-07-20 22:27:31 -0700

    Added decimal functionality

--- calculator.js
+++ calculator.js
@@ -1,27 +1,27 @@
 // ADD function, adds two nums
 function add(num1, num2) {
-    return parseInt(num1) + parseInt(num2);
+    
+    console.log(`${parseInt(num1)}`);
+    return (parseFloat(num1) + parseFloat(num2)).toFixed(5) * 1;
 }
 
 // SUBTRACT funciton, subtracts num2 from num1
 function subtract(num1, num2) {
     console.log(`subtracting`);
-    return parseInt(num1) - parseInt(num2);
+    return (parseFloat(num1) - parseFloat(num2)).toFixed(5) * 1;
 }
 
 // MULTIPLY function, returns the product of 2 nums
 function multiply(num1, num2) {
-    return parseInt(num1) * parseInt(num2);
+    return (parseFloat(num1) * parseFloat(num2)).toFixed(5) * 1;
 }
 
 // DIVIDE function, returns the quotient of 2 nums
 function divide (dividend, divisor) {
-    return parseInt(dividend) / parseInt(divisor);
+    return (parseFloat(dividend) / parseFloat(divisor)).toFixed(5) * 1;
 }
 
-//OPERATION FUNCTIONS
-
-
+//OPERATION FUNCTION
 function operate(operation) {
     let operator = operation[1];
     console.log(operator)
@@ -79,12 +79,19 @@ function numPressed(button) {
 
 function operationPressed(button) {
     currOperator = matchOperation[button.textContent];
+
     if (isOperator(equation[equation.length - 1])) {
         equation[equation.length - 1] = currOperator;
     } else {
         equation.push(currOperator);
         currNum = '0'
     }
+    console.log(`operation pressed ... eq ${equation.join('_')}`);
+    if (equation.length === 1 && isOperator(equation[0])){
+        console.log(`equation is only 1 length and has an operator`);
+        equation = [''];
+    }
+
     updateDisplayEquation();
 }
 
@@ -94,10 +101,10 @@ function isOperator(value) {
 
 function cleanEquationLastValue() {
     console.log(`cleaning eq ${equation.join(' ')}`)
-    if (isOperator(equation[equation - 1])) {
+    if (isOperator(equation[equation.length - 1])) {
         equation.pop()
     }
-    if (equation[0] === ' '){
+    if (equation[0] === ''){
         equation.shift();
     }
     return equation;
